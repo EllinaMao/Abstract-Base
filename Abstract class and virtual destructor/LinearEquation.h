1e2c6b38c4f6a0e82e091e05592a61c616bdabee
Olga <xotaoshi@gmail.com>
2025-05-21 20:14:30 +0300

    working non linear equasion

---
+++ Abstract class and virtual destructor/LinearEquation.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include <iostream>
+#include <stdexcept>
+#include "Abstract Base.h"
+#include "customExceptions.h"
+
+class LinearEquation : public Equation {
+private:
+	double mK;
+	double mB; // Коэффициенты линейного уравнения ax + b = 0
+
+public:
+	LinearEquation() :mK(0), mB(0) {}
+	LinearEquation(double k, double b) : mK(k), mB(b) {}
+
+	double roots() override {
+		if (mK == 0 && mB == 0) {
+			throw InfiniteSolutionsException();
+		}
+		if (mK == 0 && mB != 0) {
+			throw NoSolutionsException();
+		}
+		double x = -mB / mK;
+
+		return x;
+	}
+
+	
+};
---
+++ Abstract class and virtual destructor/customExceptions.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+class InfiniteSolutionsException : public std::logic_error {
+public:
+	InfiniteSolutionsException()
+		: std::logic_error("The equation has infinitely many solutions") {}
+};
+
+class NoSolutionsException
+	: public std::logic_error {
+public:
+	NoSolutionsException()
+		: std::logic_error("The equation has no solutions") {}
+};
