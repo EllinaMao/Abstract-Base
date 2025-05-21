7b5de143d1433c0d0b62162c2451823cb50cca76
Olga <xotaoshi@gmail.com>
2025-05-21 19:38:50 +0300

    Add project files.

---
+++ Abstract class and virtual destructor/Abstract Base.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <iostream>
+#include <cmath>
+#include <string>
+
+
+/*
+Создать абстрактный базовый класс с виртуальной функцией — корни уравнения.Создать производные классы : класс линейных уравнений и класс квадратных уравнений.Определить функцию вычисления корней уравнений.
+*/
+class Equation {
+public:
+	virtual void roots() = 0; 
+	virtual ~Equation() = default;
+};
